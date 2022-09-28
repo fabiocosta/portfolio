1379216a159c9b036120c0fd6adff44720b76e22
Fabio <fabiosct@gmail.com>
2022-09-28 16:30:59 -0300

    Add the Portfolio's project

---
+++ frontend_react/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+
+root.render(
+<StrictMode>
+    <App />
+    </StrictMode>
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import App from './App';
+// import './index.css';
+
+
+// ReactDOM.render(<App />, document.getElementById("root"));
